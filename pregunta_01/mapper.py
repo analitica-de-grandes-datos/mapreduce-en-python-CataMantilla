cf7cdac78707f93d40454cf84389b6c7107c4021
CataMantilla <cmantil@unal.edu.co>
2022-06-27 21:10:35 -0500

    1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+import sys
+for line in sys.stdin:
+    sys.stdout.write('{}\t1\n'.format(line.split(',')[2]))
+
